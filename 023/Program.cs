3df6a156ac8e2248631e6ca2685d88609d2c59d5
ruslan <rus.pro94@bk.ru>
2022-05-18 13:50:43 +0500

    023

---
+++ 023/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 4, 9. 
+//5 -> 1, 8, 27, 64, 125
+
+
+void GetSquar(int n)
+{
+    foreach (int ind in Enumerable.Range( 1, n ))
+    //foreach (int ind in Range(1, N))
+    System.Console.Write($"  {ind * ind * ind} ");
+    
+}
+
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string valueFromConsole = Console.ReadLine();
+
+        if (int.TryParse(valueFromConsole, out int number))
+        {
+            if (number > 0)
+                return number;
+            else
+            {
+                Console.WriteLine("Число должно быть больше 0. Введите другое число");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Нужно ввести число, отличное от нуля.");
+        }
+    }
+}
+
+string messageI = "Введите N от 1,  N=";
+int sqr = GetNumber(messageI);
+
+
+GetSquar(sqr);
